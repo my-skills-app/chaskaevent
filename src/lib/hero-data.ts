3b46959bbdad5d557355e5a44cd44ba186a559a5
Exam Saathi <apple.aryan.620501@gmail.com>
2026-01-11 17:29:46 +0530

    Remove duplicate hero data entries

--- src/lib/hero-data.ts
+++ src/lib/hero-data.ts
@@ -1,5 +1,16 @@
 
 export const heroData = [
+  
+    {
+    title: "WEDDING STAGE DECORATION",
+    subtitle: "Best Wedding Planer in Patna (Patna's No1 Wedding Plannner)",
+    imageUrl: "https://i.ibb.co/mrxQ68nn/STAGE-2.png",
+  },
+   {
+    title: "HALDI BACKDROP",
+    subtitle: "Best Wedding Planer in Patna (Patna's No1 Wedding Plannner)",
+    imageUrl: "https://i.ibb.co/ZzNFfhFB/haldi-1.png",
+  },
   {
     title: "BALLOON DECORATION EVENT",
     subtitle: "Best Wedding managment Planer in Patna (Patna's No1 Wedding Plannner)",
@@ -10,16 +21,7 @@ export const heroData = [
     subtitle: "Best Wedding Planer in Patna (Patna's No1 Wedding Plannner)",
     imageUrl: "https://i.ibb.co/0RNFPdfP/DANCE-TUP-1.png",
   },
-  {
-    title: "HALDI BACKDROP",
-    subtitle: "Best Wedding Planer in Patna (Patna's No1 Wedding Plannner)",
-    imageUrl: "https://i.ibb.co/ZzNFfhFB/haldi-1.png",
-  },
-  {
-    title: "WEDDING STAGE DECORATION",
-    subtitle: "Best Wedding Planer in Patna (Patna's No1 Wedding Plannner)",
-    imageUrl: "https://i.ibb.co/mrxQ68nn/STAGE-2.png",
-  },
+
   {
     title: "LIGHT DECORATION",
     subtitle: "Best Event Planer in Patna (Patna's No1 Wedding Plannner)",
