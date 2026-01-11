01ff77567fa687cdf7d2ed25d967e7d7ceaf8394
Exam Saathi <apple.aryan.620501@gmail.com>
2026-01-11 18:16:59 +0530

    Update portfolio-data.ts

--- src/lib/portfolio-data.ts
+++ src/lib/portfolio-data.ts
@@ -2,14 +2,14 @@ export const portfolioData = [
   {
     id: 'tech-summit',
     type: 'direct',
-    videoUrl: 'https://ddg21s9t062h4.cloudfront.net/0ku9p%2Ffile%2F562420093b7e743c753387a71ec0d1b9_378fa616d6c64a28e2c27fd83943a0e5.mp4?response-content-disposition=inline%3Bfilename%3D%22562420093b7e743c753387a71ec0d1b9_378fa616d6c64a28e2c27fd83943a0e5.mp4%22%3B&response-content-type=video%2Fmp4&Expires=1768156855&Signature=ZqmliOLQ5hBpN2p3If-4xwdGXRZPXyBPcMg9f4Ja5OrubyO2xWsa7rAFu6~pjWQn-Z07AIk4q4~8AQPGDYzL5gnikq6xA0QT6f4EyRGVnGIRPrhmVtLOg-Pp7Bp1TVtOKAT0CTuYQ7OMWLHfM1NW9U4Sdy9xAYepHxfbBEnP4J~CqKIDSZeJq7unBVLQYnH1qsm5n5uG0VOlTH-0RUkFf9tLF4gn6SwbC-AOx211DLty-2wlwu~SYZyJ1OeGD1zVZCpOXox-zHVjzgSoaoh7XTc-KAjl9wcAKXjd9S6W4UZ-Ru1XjaDYEtl3RVBt3DVM5PmY~NLf0bp1q9NC7GEqbg__&Key-Pair-Id=APKAJT5WQLLEOADKLHBQ',
+    videoUrl: 'https://d2fy7spvltzu3v.cloudfront.net/982ap%2Ffile%2F0a767c9a74452644e81a9715c53afcab_7f480db1d61260e0487aad3b0c0082b3.mp4?response-content-disposition=inline%3Bfilename%3D%220a767c9a74452644e81a9715c53afcab_7f480db1d61260e0487aad3b0c0082b3.mp4%22%3B&response-content-type=video%2Fmp4&Expires=1768157121&Signature=ZODrAw86ZPZo-q5Dd58IhU-0Lw5JwGI6u8R7-0zJS6M725bwOrDJIqHd5V3VMFHCE2b3xqt9prbirklX~lVNQCcw2kOKUqtQNHEY8NHHGTYs~0XhNGYbVWpEdvwKfRigOeqzy8crzIBqIJ6jziwEN3oRVZ-XEHaYBGOJ654p~-di4CvyBB7NyroYbULr9MWPwJKATNxBQGp8ua00LQV9gO4F2aAyfNr3pmOKVcf9EVU-dAUTlvg47JSIeTlh1FIPyIr2B1tLsXIKi~vko3cSuVgyhc6-uf1GIbM~sClt3RpxZtX9FVxLhKezBrKqeN3u9-sV1dBE0FTbob06Zy6iPw__&Key-Pair-Id=APKAJT5WQLLEOADKLHBQ',
     imageHint: 'tech conference stage',
     size: 'large',
   },
   {
     id: 'royal-wedding',
     type: 'direct',
-    videoUrl: 'videolink',
+    videoUrl: 'https://ddg21s9t062h4.cloudfront.net/0ku9p%2Ffile%2F562420093b7e743c753387a71ec0d1b9_378fa616d6c64a28e2c27fd83943a0e5.mp4?response-content-disposition=inline%3Bfilename%3D%22562420093b7e743c753387a71ec0d1b9_378fa616d6c64a28e2c27fd83943a0e5.mp4%22%3B&response-content-type=video%2Fmp4&Expires=1768156855&Signature=ZqmliOLQ5hBpN2p3If-4xwdGXRZPXyBPcMg9f4Ja5OrubyO2xWsa7rAFu6~pjWQn-Z07AIk4q4~8AQPGDYzL5gnikq6xA0QT6f4EyRGVnGIRPrhmVtLOg-Pp7Bp1TVtOKAT0CTuYQ7OMWLHfM1NW9U4Sdy9xAYepHxfbBEnP4J~CqKIDSZeJq7unBVLQYnH1qsm5n5uG0VOlTH-0RUkFf9tLF4gn6SwbC-AOx211DLty-2wlwu~SYZyJ1OeGD1zVZCpOXox-zHVjzgSoaoh7XTc-KAjl9wcAKXjd9S6W4UZ-Ru1XjaDYEtl3RVBt3DVM5PmY~NLf0bp1q9NC7GEqbg__&Key-Pair-Id=APKAJT5WQLLEOADKLHBQ',
     imageHint: 'wedding reception',
     size: 'wide',
   },
