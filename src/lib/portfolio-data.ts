6fc0dc6046ee7a96a9f4ece5783444ec40e0fae6
Exam Saathi <apple.aryan.620501@gmail.com>
2026-01-11 18:12:06 +0530

    Update portfolio-data.ts

--- src/lib/portfolio-data.ts
+++ src/lib/portfolio-data.ts
@@ -1,8 +1,8 @@
 export const portfolioData = [
   {
     id: 'tech-summit',
-    type: 'youtube',
-    videoUrl: 'https://www.youtube.com/embed/TC-OEFf-C1s?si=XDzSiXHHRbR2BhEQ',
+    type: 'direct',
+    videoUrl: 'https://ddg21s9t062h4.cloudfront.net/0ku9p%2Ffile%2F562420093b7e743c753387a71ec0d1b9_378fa616d6c64a28e2c27fd83943a0e5.mp4?response-content-disposition=inline%3Bfilename%3D%22562420093b7e743c753387a71ec0d1b9_378fa616d6c64a28e2c27fd83943a0e5.mp4%22%3B&response-content-type=video%2Fmp4&Expires=1768156855&Signature=ZqmliOLQ5hBpN2p3If-4xwdGXRZPXyBPcMg9f4Ja5OrubyO2xWsa7rAFu6~pjWQn-Z07AIk4q4~8AQPGDYzL5gnikq6xA0QT6f4EyRGVnGIRPrhmVtLOg-Pp7Bp1TVtOKAT0CTuYQ7OMWLHfM1NW9U4Sdy9xAYepHxfbBEnP4J~CqKIDSZeJq7unBVLQYnH1qsm5n5uG0VOlTH-0RUkFf9tLF4gn6SwbC-AOx211DLty-2wlwu~SYZyJ1OeGD1zVZCpOXox-zHVjzgSoaoh7XTc-KAjl9wcAKXjd9S6W4UZ-Ru1XjaDYEtl3RVBt3DVM5PmY~NLf0bp1q9NC7GEqbg__&Key-Pair-Id=APKAJT5WQLLEOADKLHBQ',
     imageHint: 'tech conference stage',
     size: 'large',
   },
