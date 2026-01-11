4de8fede31c387e8c201a09826fed95a2bb6f6b8
Exam Saathi <apple.aryan.620501@gmail.com>
2026-01-12 01:30:43 +0530

    Update portfolio-data.ts

--- src/lib/portfolio-data.ts
+++ src/lib/portfolio-data.ts
@@ -2,7 +2,7 @@ export const portfolioData = [
   {
     id: 'tech-summit',
     type: 'direct',
-    videoUrl: 'https://d2fy7spvltzu3v.cloudfront.net/982ap%2Ffile%2F0a767c9a74452644e81a9715c53afcab_7f480db1d61260e0487aad3b0c0082b3.mp4?response-content-disposition=inline%3Bfilename%3D%220a767c9a74452644e81a9715c53afcab_7f480db1d61260e0487aad3b0c0082b3.mp4%22%3B&response-content-type=video%2Fmp4&Expires=1768157121&Signature=ZODrAw86ZPZo-q5Dd58IhU-0Lw5JwGI6u8R7-0zJS6M725bwOrDJIqHd5V3VMFHCE2b3xqt9prbirklX~lVNQCcw2kOKUqtQNHEY8NHHGTYs~0XhNGYbVWpEdvwKfRigOeqzy8crzIBqIJ6jziwEN3oRVZ-XEHaYBGOJ654p~-di4CvyBB7NyroYbULr9MWPwJKATNxBQGp8ua00LQV9gO4F2aAyfNr3pmOKVcf9EVU-dAUTlvg47JSIeTlh1FIPyIr2B1tLsXIKi~vko3cSuVgyhc6-uf1GIbM~sClt3RpxZtX9FVxLhKezBrKqeN3u9-sV1dBE0FTbob06Zy6iPw__&Key-Pair-Id=APKAJT5WQLLEOADKLHBQ',
+    videoUrl: 'https://github.com/my-skills-app/chaskaevent/raw/refs/heads/main/WhatsApp%20Video%202026-01-10%20at%201.33.02%20PM.mp4',
     imageHint: 'tech conference stage',
     size: 'large',
   },
